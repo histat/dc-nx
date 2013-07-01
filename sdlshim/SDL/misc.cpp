3f7672af3c2bfe1204f5a0a19b3f2562588ea374
histat <histat.users@gmail.com>
2013-07-02 02:52:07 +0900

    Revert "gettimeofday -> ronin_gettick"
    This reverts commit bc763f22123f0c57e54a31af5306eab85bace755.

--- sdlshim/SDL/misc.cpp
+++ sdlshim/SDL/misc.cpp
@@ -5,7 +5,9 @@
 
 uint32_t SDL_GetTicks()
 {
-	return ronin_gettick();
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (uint32_t)(tv.tv_sec*1000 + tv.tv_usec/1000);
 }
 
 void SDL_Delay(int ms)
